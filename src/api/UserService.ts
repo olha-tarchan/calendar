3fc93cd22f6d36751620ac950dd99f0503d0f7ce
Olha Tarchan <olha.tarchan@gmail.com>
2021-11-30 11:27:03 +0200

    Changed url to user.js

--- src/api/UserService.ts
+++ src/api/UserService.ts
@@ -3,6 +3,6 @@ import {IUser} from "../models/IUser";
 
 export default class UserService {
     static async getUsers(): Promise<AxiosResponse<IUser[]>> {
-        return axios.get<IUser[]>('./user.json')
+        return axios.get<IUser[]>('./calendar/user.json')
     }
 }
